a503d6cce741940d3146cb61d82167903d49681b
Zorba11 <zorbathegreek111@gmail.com>
2021-08-10 09:49:56 -0600

    added input validation for name and role fields.

--- src/main/java/com/zorba11/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/zorba11/employeemanager/controller/EmployeeController.java
@@ -3,9 +3,15 @@ package com.zorba11.employeemanager.controller;
 import com.zorba11.employeemanager.entity.Employee;
 import com.zorba11.employeemanager.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -25,23 +31,35 @@ public class EmployeeController {
     }
 
     @PostMapping("api/employees")
-    public Long addEmployee(@RequestBody Employee employee) {
+    public Long addEmployee(@Valid @RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
     @PutMapping("api/employees")
-    public String updateEmployee(@RequestBody Employee employee) {
+    public String updateEmployee(@Valid @RequestBody Employee employee) {
         employeeService.updateEmployee(employee);
 
         return "Employee updated, ID: " + employee.getId();
     }
 
     @DeleteMapping("api/employees/{id}")
-
     public String deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
 
         return "Employee deleted, ID: " + id;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
 
